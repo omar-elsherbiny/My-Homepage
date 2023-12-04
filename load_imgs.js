49b1aee33cf3fb5fe0eedb1a06e790b9f0ae9df7
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2023-12-04 16:48:22 +0200

    lazy load imgs fix

--- load_imgs.js
+++ load_imgs.js
@@ -3,8 +3,10 @@ for (let i=1;i<=12;i++) {
     const image = document.createElement('img');
     image.classList.toggle('pic');
     image.style.marginTop='100vh';
+    image.style.opacity='0';
     image.onload = function () {
         image.style.marginTop='2rem';
+        image.style.opacity='1';
     };
     image.src='./imgs/' + i + '.jpg';
     image.onclick = function () {
